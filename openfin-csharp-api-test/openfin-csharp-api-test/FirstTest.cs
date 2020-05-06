16beb3c2bb1e4e35fc7280d322ee815cd497d45a
XuluWarrior <github@metaliving.plus.com>
2020-05-06 17:54:25 +0100

    Basic OpenFin open/close test

---
+++ openfin-csharp-api-test/openfin-csharp-api-test/FirstTest.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenfinDesktop
+{
+    class FirstTest
+    {
+        ChromeDriver driver;
+
+        [SetUp]
+        public void StartBrowser()
+        {
+            var options = new ChromeOptions();
+            string dir = Path.GetDirectoryName(GetType().Assembly.Location);
+            options.BinaryLocation = Path.Combine(dir, "RunOpenFin.bat");
+            options.AddArgument("--config=http://localhost:9070/app.json");
+            options.AddArgument("--remote-debugging-port=4444");
+            driver = new ChromeDriver(options);
+        }
+
+        [Test]
+        public void Test()
+        {
+            driver.ExecuteScript("alert('My first test')");
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            // Neither of the below actually close the OpenFin runtime
+            //driver.Close();
+            //driver.Quit();
+            driver.ExecuteScript("window.close()"); // This does
+        }
+    }
+}
