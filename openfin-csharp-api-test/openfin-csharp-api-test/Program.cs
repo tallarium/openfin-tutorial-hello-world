5216716c15500de687ebbb80b8b23a3ac7ab8f4e
XuluWarrior <github@metaliving.plus.com>
2020-05-06 13:23:52 +0100

    Openfin runtime 14.78.46.23

--- openfin-csharp-api-test/openfin-csharp-api-test/Program.cs
+++ openfin-csharp-api-test/openfin-csharp-api-test/Program.cs
@@ -15,7 +15,7 @@ namespace OpenfinDesktop
         {
             var taskCompletionSource = new TaskCompletionSource<bool>();
             RuntimeOptions options = new RuntimeOptions();
-            options.Version = "9";
+            options.Version = "14.78.46.23";
             Runtime runtime = Openfin.Desktop.Runtime.GetRuntimeInstance(options);
             runtime.Connect(() =>
             {
