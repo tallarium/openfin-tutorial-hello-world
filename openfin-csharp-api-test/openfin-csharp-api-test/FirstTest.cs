c07ebb28e02051e05a72f655e401dcccaf46aa6b
XuluWarrior <github@metaliving.plus.com>
2020-05-12 11:47:41 +0100

    IsRunning tests TODO: IsRunningInitiallyClosed fails

--- openfin-csharp-api-test/openfin-csharp-api-test/FirstTest.cs
+++ openfin-csharp-api-test/openfin-csharp-api-test/FirstTest.cs
@@ -1,4 +1,5 @@
-using NUnit.Framework;
+﻿using NUnit.Framework;
+using Openfin.Desktop;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Remote;
@@ -25,11 +26,79 @@ namespace OpenfinDesktop
             driver = new ChromeDriver(options);
         }
 
+        private Task<Runtime> ConnectToRuntime()
+        {
+            var taskCompletionSource = new TaskCompletionSource<Runtime>();
+
+            RuntimeOptions options = new RuntimeOptions();
+            options.Version = "14.78.46.23";
+            Runtime runtime = Openfin.Desktop.Runtime.GetRuntimeInstance(options);
+            runtime.Connect(() =>
+            {
+                taskCompletionSource.SetResult(runtime);
+            });
+
+            return taskCompletionSource.Task;
+        }
+
+        private Task<bool> AppIsRunning(Application app)
+        {
+            var taskCompletionSource = new TaskCompletionSource<bool>();
+
+            app.isRunning((Ack ack) =>
+            {
+                bool isRunning = ack.getJsonObject().Value<bool>("data");
+                taskCompletionSource.TrySetResult(isRunning);
+            }, (Ack ack) =>
+            {
+                // Error
+            });
+            return taskCompletionSource.Task;
+        }
+
         [Test]
         public void Test()
         {
             StartOpenfinApp();
             driver.ExecuteScript("alert('My first test')");
+        }
+
+        // TODO: This test fails as ChromeDriver fails to connect to OpenFin
+        [Test]
+        public async Task IsRunningInitiallyClosed()
+        {
+            
+            Runtime runtime = await ConnectToRuntime();
+            Application app = runtime.WrapApplication("openfin-closing-events-demo");
+            bool isRunning = await AppIsRunning(app);
+
+            Assert.IsFalse(isRunning);
+            StartOpenfinApp();
+            isRunning = await AppIsRunning(app);
+            Assert.IsTrue(isRunning);
+            StopOpenfinApp();
+            isRunning = await AppIsRunning(app);
+            Assert.IsFalse(isRunning);
+        }
+
+        [Test]
+        public async Task IsRunningInitiallyOpen()
+        {
+
+            StartOpenfinApp();
+
+            Runtime runtime = await ConnectToRuntime();
+            Application app = runtime.WrapApplication("openfin-closing-events-demo");
+
+            bool isRunning = await AppIsRunning(app);
+
+            Assert.IsTrue(isRunning);
+            StopOpenfinApp();
+            isRunning = await AppIsRunning(app);
+            Assert.IsFalse(isRunning);
+            StartOpenfinApp();
+            isRunning = await AppIsRunning(app);
+            Assert.IsTrue(isRunning);
         }
 
         [TearDown]
