5a4beadabcb01e29347703a5b1df578c437d6338
XuluWarrior <github@metaliving.plus.com>
2020-05-21 14:30:34 +0100

    Using Platforms, app.getWindow() does not return the actual app window

--- openfin-csharp-api-test/openfin-csharp-api-test/OpenfinTests.cs
+++ openfin-csharp-api-test/openfin-csharp-api-test/OpenfinTests.cs
@@ -111,17 +111,53 @@ alert('Resizing')
 const app = fin.Application.getCurrentSync();
 const win = await app.getWindow();
 const bounds = {{
-    height: 100,
+    height: 200,
     width: 200,
     top: 400,
     left: 400
 }}
-await win.setBounds(bounds);
+await win.setBounds(bounds);  // Doesn't resize app window as win isn't the app window.  See AppHasDefaultWidth
 alert('Resized');
 ";
             driver.ExecuteScript(script);
         }
 
+        [Test]
+        public void AppHasDefaultWidth()
+        {
+            StartOpenfinApp();
+            var script = $@"
+const app = fin.Application.getCurrentSync();
+const win = await app.getWindow();
+const origBounds = await win.getBounds();
+return origBounds;
+";
+            var result = driver.ExecuteScript(script);
+            Dictionary<string, object> bounds = (Dictionary<string, object>)result;
+            Assert.AreEqual(600, bounds["width"]); // Fails as win is not the app window and has it's own dimensions
+            Assert.AreEqual(600, bounds["height"]);
+        }
+
+        [Test]
+        public void ResizeChildWindow()
+        {
+            StartOpenfinApp();
+            var script = $@"
+alert('Resizing')
+const app = fin.Application.getCurrentSync();
+const bounds = {{
+    height: 200,
+    width: 200,
+    top: 400,
+    left: 400
+}}
+const childWindows = await app.getChildWindows()
+const childWin = childWindows[0];
+await childWin.setBounds(bounds);  // This is the actual app window and resize works.
+alert('Resized childWin');
+";
+            driver.ExecuteScript(script);
+        }
         public void StopOpenfinApp()
         {
             if (driver != null)
