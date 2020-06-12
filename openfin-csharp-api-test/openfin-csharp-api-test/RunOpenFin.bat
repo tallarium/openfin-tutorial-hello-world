20e716a146f80a438d94db229d464c27ca716db5
Adrian Williams <adrian@tallarium.com>
2020-06-12 11:53:14 +0100

    More meaningful debug messages

--- openfin-csharp-api-test/openfin-csharp-api-test/RunOpenFin.bat
+++ openfin-csharp-api-test/openfin-csharp-api-test/RunOpenFin.bat
@@ -1,5 +1,6 @@
 @ECHO OFF
-REM This script is intended to be used for launching OpenFin Runtime by ChromeDriver or node of Selenium Grid server.
+REM This script is originally from https://github.com/openfin/hello-openfin-selenium-example
+REM It is intended to be used for launching OpenFin Runtime by ChromeDriver or node of Selenium Grid server.
 REM It parses --remote-debugging-port and --config parameters passed by ChromeDriver and passes them to OpenFinRVM.exe
 REM %openfinLocation% needs to point to location of OpenFin install directory.
 REM devtools_port should NOT be set in app manifest json file
@@ -24,8 +25,9 @@ SET debuggingPort=0
 
 :cont
 
-echo %debuggingPort%
-echo %startupURL%
+
+echo Starting %startupURL%
+echo Debugging port: %debuggingPort%
 
 SET openfinLocation=%LocalAppData%\OpenFin
 c:
